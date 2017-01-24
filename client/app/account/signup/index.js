6180a9737f11fe712ed8deb6856a19c9c378915a
Doug Drenkow <doug@douglasdrenkow.com>
2017-01-24 07:39:41 -0800

    Commit everything copied.

---
+++ client/app/account/signup/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import angular from 'angular';
+import SignupController from './signup.controller';
+
+export default angular.module('dedyongfull3App.signup', [])
+  .controller('SignupController', SignupController)
+  .name;
